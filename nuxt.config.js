fcc7b76c8005ea1066814449b843d645f854bc42
mel0620 <cunetarommel0620@gmail.com>
2021-10-09 01:20:32 +0800

    done calc

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - atphloancalc',
-    title: 'atphloancalc',
+    title: 'AutoTrustPH Loan Calculator',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -15,7 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', href: '/logo.png' }
     ]
   },
 
